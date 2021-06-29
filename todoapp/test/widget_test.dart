852eb944dc789a7055cba18611944d4840840197
raymondddenny <dennyraymondd@gmail.com>
2021-06-29 14:37:33 +0700

    init project and create cubit for todos

--- todoapp/test/widget_test.dart
+++ todoapp/test/widget_test.dart
@@ -9,11 +9,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:todoapp/main.dart';
+import 'package:todoapp/presentation/router.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TodoApp(
+      router: AppRouter(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
