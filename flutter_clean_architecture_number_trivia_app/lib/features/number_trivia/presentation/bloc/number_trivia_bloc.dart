4607640ca8d32c3c99f706139dbf1addce635d21
raymondddenny <dennyraymondd@gmail.com>
2021-07-01 14:35:21 +0700

    feat(bloc scaffolding & Input conversion): Setup Bloc, handle input and the test

---
+++ flutter_clean_architecture_number_trivia_app/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_clean_architecture_number_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(NumberTriviaInitial());
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
